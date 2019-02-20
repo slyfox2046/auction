e918baeb674aee4698dbbec77369ad3f583d583a
slyfox <slyfox2046@gmail.com>
2019-02-20 17:51:34 +0800

    test

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,19 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
   private products:Array<Product>;
 
+  private imgUrl = "http://placehold.it/320x150";
+
   constructor() { }
 
   ngOnInit() {
     this.products = [
-      new Product(1,"第一个商品",1.99,3.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
-      new Product(2,"第二个商品",2.99,2.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["图书"]),
-      new Product(3,"第三个商品",3.99,4.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["硬件设备"]),
-      new Product(4,"第四个商品",4.99,1.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
-      new Product(5,"第五个商品",5.99,3.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品"]),
-      new Product(6,"第六个商品",6.99,2.5,"这是第一个商品，是我在学习慕课网Angular入门实战时创建的",["图书"])
+      new Product(1,"第一个商品",1.99,3.5,"这是第1个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
+      new Product(2,"第二个商品",2.99,2.5,"这是第2个商品，是我在学习慕课网Angular入门实战时创建的",["图书"]),
+      new Product(3,"第三个商品",3.99,4.5,"这是第3个商品，是我在学习慕课网Angular入门实战时创建的",["硬件设备"]),
+      new Product(4,"第四个商品",4.99,1.5,"这是第4个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品","硬件设备"]),
+      new Product(5,"第五个商品",5.99,3.5,"这是第5个商品，是我在学习慕课网Angular入门实战时创建的",["电子产品"]),
+      new Product(6,"第六个商品",6.99,2.5,"这是第6个商品，是我在学习慕课网Angular入门实战时创建的",["图书"])
 
     ]
+    this.products.push(new Product(7,"第七个商品",4.99,3.5,"这是第7个商品，是我在学习慕课网Angular入门实战时创建的",["图书"]) )
   }
+
+
 
 }
 
