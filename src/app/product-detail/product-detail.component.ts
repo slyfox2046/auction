65f13a74daedffccdaa8ec3b6d9bbdb6cd267fa4
slyfox <slyfox2046@gmail.com>
2019-02-26 15:37:29 +0800

    test

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -11,6 +11,10 @@ export class ProductDetailComponent implements OnInit {
   // productTitle:string;
   product:Product;
   comments:Comment[];
+  newRating :number =5;
+  newComment :string ="";
+
+  isCommentHidden = true;
 
   constructor(private routeInfo:ActivatedRoute,
               private productService:ProductService) { }
@@ -21,6 +25,22 @@ export class ProductDetailComponent implements OnInit {
     this.product = this.productService.getPdoduct(productId);
 
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment(){
+    let comment = new Comment(0,this.product.id,new Date().toISOString(),"SomeOne",this.newRating,this.newComment);
+    this.comments.unshift(comment);
+
+    let sum = this.comments.reduce((sum,comment)=>sum+comment.rating,0);
+    this.product.rating = sum/this.comments.length;
+
+    //恢复成默认值
+    this.newComment= null;
+    this.newRating = 5;
+
+  //  提交后算平均星集
+
+
   }
 
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,24 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit,OnChanges {
+  // 注意输入属性和输出属性的名称要相同 rating
   @Input()
   private rating:number =0;
+  @Output()
+  private ratingChange : EventEmitter<number> = new EventEmitter();
 
   private stars :boolean[];
+  @Input()
+  private readonly :boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+  ngOnChanges (changes: SimpleChanges): void {
+    // 原来代码放在OnInit中，现在放这里是当输入属性rating改变时候，跟新stars的数组
     this.stars=[];
     for (let i=1;i<=5;i++) {
       this.stars.push(i>this.rating)
     }
     // this.stars = [true,true,false,true,true]
+  }
+
+  clickStar(index :number){
+    //index 是从0开始的
+    if (!this.readonly){
+      this.rating=index+1;
+      this.ngOnInit();
+      this.ratingChange.emit(this.rating);
+    }
   }
 
 }
